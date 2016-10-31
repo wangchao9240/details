02eac23ed0ce146a9fa129b0eb7fee846fe2cef3
1603qd <582606942@qq.com>
2016-10-31 08:30:22 +0800

    Update gulpfile.js

--- autoCompile/gulpfile.js
+++ autoCompile/gulpfile.js
@@ -1,7 +1,14 @@
 var gulp=require("gulp")
-var connect=require("gulp-connect")
-var watch=require("gulp-watch")
-var sass=require("gulp-sass")
+var connect=require("gulp-connect")		//重刷新页面
+var watch=require("gulp-watch")			//页面检测
+var sass=require("gulp-sass")			//sass编译
+var rev=require("gulp-rev")			//打版本号
+var revReplace=require("gulp-rev-replace")	//版本号重引用
+var useref=require("gulp-useref")		//抓取页面要合并的文件
+var filter=require("gulp-filter")		//过滤器
+var uglify=require("gulp-uglify")		//js打包
+var csso=require("gulp-csso")			//css打包
+var imageMin=require("gulp-imagemin")		//图片压缩
 
 gulp.task("connect",function(){
 	connect.server({
@@ -34,3 +41,38 @@ gulp.task("watch",function(){
 })
 //------监听部分结束----------
 gulp.task("default",["connect","watch","sass:watch"])
+
+// --------------打包部分------------------
+gulp.task("package",function(){
+	var jsFilter=filter(["**/*.js"],{"restore":"true"})
+	var cssFilter=filter(["**/*.css"],{"restore":"true"})
+	var indexHtmlFliter=filter(["**/*","!src/homePage/homePage.html"],{"restore":"true"})
+	return gulp.src("src/homePage/homePage.html")
+		.pipe(useref())
+		.pipe(jsFilter)
+		.pipe(uglify())
+		.pipe(jsFilter.restore)
+		.pipe(cssFilter)
+		.pipe(csso())
+		.pipe(cssFilter.restore)
+		.pipe(indexHtmlFliter)
+		.pipe(rev())
+		.pipe(indexHtmlFliter.restore)
+		.pipe(revReplace())
+		.pipe(gulp.dest("./combined"))
+})
+
+gulp.task("imageMin",function(){
+	return gulp.src("src/homePage/img/*")
+		.pipe(imageMin())
+		.pipe(gulp.dest("./combined/img"))
+})
+
+gulp.task("fontMove",function(){
+	return gulp.src(["src/homePage/font/iconfont.*","!src/homePage/font/iconfont.css"])
+			.pipe(gulp.dest("./combined/css"))
+})
+
+gulp.task("packageBag",["package","imageMin","fontMove"])
+
+// ------------打包部分结束-------------------------
